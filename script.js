ac01bdb4fa18be0de9873042bc121bb439576b89
João Tarzan <joao.extremo722@gmail.com>
2022-06-07 21:15:24 -0300

    Carregamento de mapa e marcar e apagar local

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
+var marcarLoc;
+var removeLoc;
+
+function localizar() {
+	navigator.geolocation.getCurrentPosition(showPosition, showError, {
+	enableHighAccuracy:true
+}) 
+}
+
+function showPosition(pos) {
+	let lat = pos.coords.latitude;
+	let lng = pos.coords.longitude;
+	console.log('latitude: '+ lat, ' longitude: ' + lng);
+
+	var map = L.map('map').setView([lat, lng], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGV2LWluaWNpYW50ZSIsImEiOiJjbDMxaGhkYjQwMWkyM2tuMjQyZXJ4d3A1In0.3ZLAG3knntfCNSSzoCrsnQ'
+	}).addTo(map);
+
+	var marker = L.marker([lat, lng]);
+
+	marcarLoc = () => {
+		map.setView([lat, lng], 13);
+		marker.addTo(map)
+		marker.bindPopup("<b>Ola!</b><br>Você esta aqui!.").openPopup();
+	}
+
+	removeLoc = () => {
+		map.removeLayer(marker);
+	}
+	
+	
+}
+
+
+function showError(err){
+	alert(err.message + ", " + err.code);
+}
+
+
+localizar();
+
